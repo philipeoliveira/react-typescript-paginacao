9d5a1bc97b56577bd6d71db35eccbe2e93730bc4
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-07 16:41:01 -0300

    Add title to buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,16 +3,18 @@ import { ReactNode } from 'react';
 interface ButtonProps {
    children: ReactNode;
    onClick: () => void;
+   title: string;
    isDisabled: boolean;
 }
 
-export function Button({ children, onClick, isDisabled }: ButtonProps) {
+export function Button({ children, onClick, title, isDisabled }: ButtonProps) {
    return (
       <button
          type='button'
-         className='bg-zinc-400 border border-zinc-950 text-zinc-950 rounded-lg p-1 hover:bg-zinc-300 disabled:bg-zinc-700'
+         className='bg-zinc-400 border border-zinc-950 text-zinc-950 rounded-lg p-1.5 hover:bg-zinc-300 disabled:bg-zinc-700'
          onClick={onClick}
          disabled={isDisabled}
+         title={title}
       >
          {children}
       </button>
