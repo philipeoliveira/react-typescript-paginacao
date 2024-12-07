7680c98091c87e02e89a8fc473d6f395f6875801
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-07 16:43:07 -0300

    Changes the location of the pagination buttons

--- src/components/UsersPagination.tsx
+++ src/components/UsersPagination.tsx
@@ -18,52 +18,56 @@ export function UsersPagination() {
       goToLastPage,
    } = usePagination(data);
 
+   const currentItems = data.slice((page - 1) * 10, page * 10);
+
    return (
-      <Table>
-         <thead>
-            <TableRow>
-               <TableHeader>Código</TableHeader>
-               <TableHeader>Nome</TableHeader>
-               <TableHeader>Sobrenome</TableHeader>
-               <TableHeader otherClasses='max-sm:hidden'>E-mail</TableHeader>
-            </TableRow>
-         </thead>
-         <tbody>
-            {data.slice((page - 1) * 10, page * 10).map((item) => {
-               return (
-                  <TableRow key={item.id}>
-                     <TableCell>{item.id}</TableCell>
-                     <TableCell>{item.firstName}</TableCell>
-                     <TableCell>{item.lastName}</TableCell>
-                     <TableCell otherClasses='max-sm:hidden'>{item.email}</TableCell>
-                  </TableRow>
-               );
-            })}
-         </tbody>
-         <tfoot>
-            <tr>
-               <TableCell colSpan={2} otherClasses='text-sm'>
-                  10 de {data.length}
-               </TableCell>
-               <TableCell colSpan={2} otherClasses='text-sm text-right'>
-                  <div className='flex max-sm:flex-col items-center justify-end gap-4 py-3'>
-                     <span>
-                        Página {page} de {totalPages}
-                     </span>
-                     <Pagination
-                        pagination={{
-                           page,
-                           totalPages,
-                           goToFirstPage,
-                           goToNextPage,
-                           goToPreviousPage,
-                           goToLastPage,
-                        }}
-                     />
-                  </div>
-               </TableCell>
-            </tr>
-         </tfoot>
-      </Table>
+      <>
+         <Table>
+            <thead>
+               <TableRow>
+                  <TableHeader>Código</TableHeader>
+                  <TableHeader>Nome</TableHeader>
+                  <TableHeader>Sobrenome</TableHeader>
+                  <TableHeader otherClasses='max-sm:hidden'>E-mail</TableHeader>
+               </TableRow>
+            </thead>
+            <tbody>
+               {currentItems.map((item) => {
+                  return (
+                     <TableRow key={item.id}>
+                        <TableCell>{item.id}</TableCell>
+                        <TableCell>{item.firstName}</TableCell>
+                        <TableCell>{item.lastName}</TableCell>
+                        <TableCell otherClasses='max-sm:hidden'>{item.email}</TableCell>
+                     </TableRow>
+                  );
+               })}
+            </tbody>
+            <tfoot>
+               <tr>
+                  <TableCell colSpan={2} otherClasses='text-sm'>
+                     {currentItems.length} de {data.length}
+                  </TableCell>
+                  <TableCell colSpan={2} otherClasses='text-sm text-right'>
+                     <div className='flex max-sm:flex-col items-center justify-end gap-4 py-3'>
+                        <span>
+                           Página {page} de {totalPages}
+                        </span>
+                     </div>
+                  </TableCell>
+               </tr>
+            </tfoot>
+         </Table>
+         <Pagination
+            pagination={{
+               page,
+               totalPages,
+               goToFirstPage,
+               goToNextPage,
+               goToPreviousPage,
+               goToLastPage,
+            }}
+         />
+      </>
    );
 }
