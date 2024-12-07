a041b005f5505f47e7046772fe67b40cd1bc8747
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-07 16:43:55 -0300

    Add title for buttons and centralizes

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -21,17 +21,29 @@ export function Pagination({ pagination }: { pagination: PaginationProps }) {
    } = pagination;
 
    return (
-      <div className='flex gap-0.5'>
-         <Button onClick={goToFirstPage} isDisabled={page === 1}>
+      <div className='flex justify-center gap-0.5'>
+         <Button onClick={goToFirstPage} isDisabled={page === 1} title='Primeira página'>
             <ChevronsLeft size={18} />
          </Button>
-         <Button onClick={goToPreviousPage} isDisabled={page === 1}>
+         <Button
+            onClick={goToPreviousPage}
+            isDisabled={page === 1}
+            title='Página anterior'
+         >
             <ChevronLeft size={18} />
          </Button>
-         <Button onClick={goToNextPage} isDisabled={page === totalPages}>
+         <Button
+            onClick={goToNextPage}
+            isDisabled={page === totalPages}
+            title='Próxima página'
+         >
             <ChevronRight size={18} />
          </Button>
-         <Button onClick={goToLastPage} isDisabled={page === totalPages}>
+         <Button
+            onClick={goToLastPage}
+            isDisabled={page === totalPages}
+            title='Última página'
+         >
             <ChevronsRight size={18} />
          </Button>
       </div>
